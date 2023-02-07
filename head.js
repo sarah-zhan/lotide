60361a37e0c13ca324cd246eedd4ca4446d391a8
sarah-zhan <yizh.contact@gmail.com>
2023-02-07 08:23:06 -0800

    equal function

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 😀 😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😔 😔 😔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+let array = [];
+const head = function (array) {
+  
+  return array[0];
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([2]), 2);
+assertEqual(head([]), 0);
+
