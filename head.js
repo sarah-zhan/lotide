4f0d52effecb7d8ec3025e7f770e80c9d3c1a46b
sarah-zhan <yizh.contact@gmail.com>
2023-02-08 15:27:16 -0800

    add without function

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😀 😀 😀 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -8,10 +8,10 @@ const assertEqual = function (actual, expected) {
 };
 
 let array = [];
-const head = function (array) {
+const head = function(array) {
   
   return array[0];
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
---
+++ without.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("They are a match.");
+  } else {
+    console.log("Not a match!");
+  }
+};
+
+
+
+const without = function(arr, itemsToRemove) {
+  if (!Array.isArray(arr) || !Array.isArray(itemsToRemove)) {
+    return [];
+  }
+  let output;
+
+  output = arr.filter(word => {
+    if (!itemsToRemove.includes(word)) {
+      return word;
+    }
+  });
+  return output;
+};
+
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+console.log(words);
+// Make sure the original array was not altered by the without function
+assertArrayEqual(without(words, ["lighthouse"]), ["hello", "world"]);
