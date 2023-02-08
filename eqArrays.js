51439fff23c235d301e5cedf14d15d0369f6b918
sarah-zhan <yizh.contact@gmail.com>
2023-02-08 09:20:35 -0800

    eslint

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😀 😀 😀 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,17 +7,17 @@ const assertEqual = function (actual, expected) {
 
 };
 
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) { 
+      if (array1[i] !== array2[i]) {
         return false;
       }
     }
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
