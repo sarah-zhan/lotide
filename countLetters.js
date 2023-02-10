a3fb47c3bfcea48823b821459ae64739590685a8
sarah-zhan <yizh.contact@gmail.com>
2023-02-09 21:45:29 -0800

    map function

--- countLetters.js
+++ countLetters.js
@@ -12,8 +12,7 @@ const countLetters = function(sentence) {
     if (letter !== ' ') {
       if (results[letter]) {
         results[letter] += 1;
-      }
-      else {
+      } else {
         results[letter] = 1;
       }
     }
@@ -24,14 +23,14 @@ const countLetters = function(sentence) {
 console.log(countLetters("lighthouse in the house"));
 //{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
 
-assertEqual(countLetters("lighthouse in the house").l, 1); 
-assertEqual(countLetters("lighthouse in the house").i, 2); 
-assertEqual(countLetters("lighthouse in the house").g, 1); 
-assertEqual(countLetters("lighthouse in the house").h, 4); 
-assertEqual(countLetters("lighthouse in the house").t, 2); 
-assertEqual(countLetters("lighthouse in the house").o, 2); 
-assertEqual(countLetters("lighthouse in the house").u, 2); 
-assertEqual(countLetters("lighthouse in the house").s, 2); 
-assertEqual(countLetters("lighthouse in the house").e, 3); 
-assertEqual(countLetters("lighthouse in the house").n, 1); 
+assertEqual(countLetters("lighthouse in the house").l, 1);
+assertEqual(countLetters("lighthouse in the house").i, 2);
+assertEqual(countLetters("lighthouse in the house").g, 1);
+assertEqual(countLetters("lighthouse in the house").h, 4);
+assertEqual(countLetters("lighthouse in the house").t, 2);
+assertEqual(countLetters("lighthouse in the house").o, 2);
+assertEqual(countLetters("lighthouse in the house").u, 2);
+assertEqual(countLetters("lighthouse in the house").s, 2);
+assertEqual(countLetters("lighthouse in the house").e, 3);
+assertEqual(countLetters("lighthouse in the house").n, 1);
 
---
+++ map.js
@@ -0,0 +1,39 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+
+  }
+  return results;
+}
+
+const assertArrayEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("They are a match.");
+  } else {
+    console.log("Not a match!");
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// const results1 = map(words, word => word[0]);
+// console.log(results1);
+
+assertArrayEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArrayEqual(map(words, word => word[0])[0], ['g']);
+assertArrayEqual(map(words, word => word[0])[1], ['c']);
+assertArrayEqual(map(words, word => word[0])[2], ['t']);
+assertArrayEqual(map(words, word => word[0])[3], ['m']);
+assertArrayEqual(map(words, word => word[0])[4], ['t']);
