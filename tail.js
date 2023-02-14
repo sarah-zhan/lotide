2f2adcb59cb1cf5bb46fca6ff536f653ac5f1e90
sarah-zhan <yizh.contact@gmail.com>
2023-02-13 17:12:02 -0800

    refactor use export module

--- tail.js
+++ tail.js
@@ -1,19 +1,13 @@
-const assertEqual = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] === expected[i]) {
-      console.log(`😀 😀 😀 Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`😔 😔 😔 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 const tail = function(array1) {
   return array1.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+
+
+module.exports = tail;
 
 
 
