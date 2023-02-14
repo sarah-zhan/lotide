4793550e2c3c8a448adbd3d22c631a44eafd8dad
sarah-zhan <yizh.contact@gmail.com>
2023-02-13 17:44:02 -0800

    add testing

---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+const assertArrayEqual = require('../assertArrayEqual');
+
+assertArrayEqual([1, 2, 3], [3, 2, 1]);
+assertArrayEqual([3, 2, 1], [3, 2, 1]);
